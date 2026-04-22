60750530e20847b826bc315293900684a678c0fa
ApekshaPai18 <91828694+ApekshaPai18@users.noreply.github.com>
2026-04-22 20:22:15 +0530

    removed the unused vars

--- frontend/src/pages/Lesson.tsx
+++ frontend/src/pages/Lesson.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   Container,
   Paper,
@@ -13,8 +13,8 @@ import {
   DialogContent,
   DialogActions,
   Snackbar,
-  IconButton,
-  CircularProgress
+  IconButton
+  
 } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
